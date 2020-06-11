c11feb01341555de27a355e67dc2b9f2efe6dc62
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 18:18:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
 def reverse_each_word(sentence)
-  =begin
-  sentence_array = sentence.split(/ /)
-  reverse_sentence = ""
-  sentence_array.each { |word|
-    reverse_sentence << word.reverse + " "
-  }
-  reverse_sentence.strip
-  =end
-
 
   sentence_array = sentence.split(/ /)
   reverse_array = sentence_array.collect do |word|
@@ -15,3 +6,12 @@ def reverse_each_word(sentence)
   end
   reverse_array.join(" ")
 end
+
+=begin
+sentence_array = sentence.split(/ /)
+reverse_sentence = ""
+sentence_array.each { |word|
+  reverse_sentence << word.reverse + " "
+}
+reverse_sentence.strip
+=end

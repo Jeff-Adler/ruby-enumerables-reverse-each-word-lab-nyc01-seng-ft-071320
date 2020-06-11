5d1231a36335f4f24313c297fb7f92e5e5f9f5c5
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 18:06:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
-  sentence_array.each_char { |word|
+  sentence_array.each { |word|
     word = word.reverse
   }
   sentence_array
